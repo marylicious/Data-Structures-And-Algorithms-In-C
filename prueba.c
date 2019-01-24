e911a1409e1cb407adb4acb6650fd2ec272fcb7d
Maria V Ortiz <mariatua2@gmail>
2019-01-23 20:34:32 -0400

    printing variables

---
+++ prueba.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//#include <stdlib.h> //necesario para hacer pause con windows
+
+int main () {
+    printf("Hola \n"); //console.log de c
+   // system("PAUSE");
+    return 1;
+}
---
+++ video2.c
@@ -0,0 +1,11 @@
+//identificadores y tipos
+#include <stdio.h>
+
+int main () {
+    char letra = 'A';
+    char letra2 = 'B';
+    int entero = 200;
+    float flo = 123.1231;
+
+    printf("Aqui imprimire letra %c, letra2 %c, entero %i,  un float %f \n", letra,letra2,entero,flo);
+}
