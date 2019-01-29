c07e5a48f74d65c26b19b77bf4314a118e479276
Maria V <mariatua2@gmail.com>
2019-01-29 16:21:29 -0400

    adding struct

---
+++ Sintaxis/11_Struct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+//Struct es como los registros en pascal.
+
+struct Persona {
+    unsigned short int edad;
+    char *nombre;
+};
+
+struct Carro {
+    char *dueno;
+    unsigned short int year;
+};
+
+typedef struct Carro CA; //se define el tipo CA
+
+int main () {
+    
+    struct Persona maria;
+        
+    maria.edad=22;
+    maria.nombre = "Maria V";
+
+    printf("%s tiene %i a#os \n",maria.nombre,maria.edad);
+
+    CA carro;
+    carro.dueno = maria.nombre;
+    carro.year = 96;
+
+    printf("El carro de %s es del %i \n",carro.dueno,carro.year);
+
+    return 0;
+}
